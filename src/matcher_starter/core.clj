8a7d08825529cf5895cdbba2aab44e1e2287694e
ALEX COWIE <V8029617@live.tees.ac.uk>
2020-05-08 16:57:42 +0100

    removed blanklines and added comments

--- src/matcher_starter/core.clj
+++ src/matcher_starter/core.clj
@@ -8,28 +8,22 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
-(def state1 '#{(at lift ground)
-               (at Person fifth)
-               ;(going Person)
-
-               (waiting Person false)
-               (moving lift false)
-               (contains lift nil)})
-(def world
+(def state-alex
+  '#{(at lift ground)
+     (at Person fifth)
+     (waiting Person false)
+     (moving lift false)
+     (contains lift nil)})
+(def world-alex
   '#{
-
-     ;(selected lift ground)
-
      (container  lift)
      (agent Person)
-
      (place ground)
      (place first)
      (place second)
      (place third)
      (place fourth)
      (place fifth)
-     ;(place destination)
      (above ground first)
      (above first second)
      (above second third)
@@ -40,7 +34,6 @@
      (below third second)
      (below fourth third)
      (below fifth fourth)
-
      }
   )
 
@@ -53,7 +46,6 @@
                       (moving lift false)
                       (at ?lift ?floor)
                       (at ?P ?p-floor))
-
                 :add((moving ?lift true))
                 :del((moving ?lift false))
                 :txt(?P called ?lift from ?p-floor)
@@ -62,112 +54,96 @@
     going-up{;moving the lift when it is not occupied
              :pre((container ?lift)
                   (contains ?lift nil)
-                    (agent ?P)
-                    (waiting ?P true)
-                    (moving ?lift true)
-                    (at ?lift ?floor)
-                    (above ?floor ?above))
-              :add((at ?lift ?above))
-              :del((at ?lift ?floor))
-              :txt(?lift moves up from ?floor to ?above)
-              :cmd(ascend lift)
-
+                  (agent ?P)
+                  (waiting ?P true)
+                  (moving ?lift true)
+                  (at ?lift ?floor)
+                  (above ?floor ?above))
+             :add((at ?lift ?above))
+             :del((at ?lift ?floor))
+             :txt(?lift moves up from ?floor to ?above)
+             :cmd(ascend lift)
               }
     going-down{;moving the lift when not occupied
                :pre((container ?lift)
                     (contains ?lift nil)
-                     (agent ?P)
-                     (waiting ?P true)
-                     (moving ?lift true)
-                     (at ?lift ?floor)
-                     (below ?floor ?below))
-                :add((at ?lift ?below))
-                :del((at ?lift ?floor))
-                :txt(?lift moves down from ?floor to ?below)
-                :cmd(descend lift)
-
+                    (agent ?P)
+                    (waiting ?P true)
+                    (moving ?lift true)
+                    (at ?lift ?floor)
+                    (below ?floor ?below))
+               :add((at ?lift ?below))
+               :del((at ?lift ?floor))
+               :txt(?lift moves down from ?floor to ?below)
+               :cmd(descend lift)
                 }
     going-up-filled{;moving the occupant up one floor
                     :pre((container ?lift)
-                          (agent ?P)
-                          (contains ?lift ?P)
-                          (moving ?lift true)
+                         (agent ?P)
+                         (contains ?lift ?P)
+                         (moving ?lift true)
                          (waiting ?P true)
-                          (at ?lift ?floor)
-                          (above ?floor ?above))
-                     :add((at ?lift ?above)
-                          (at ?P ?above))
-                     :del((at ?lift ?floor)
-                          (at ?P ?floor))
-                     :txt(?lift moves ?P up from ?floor to ?above)
-                     :cmd(ascend person)
-
-                     }
+                         (at ?lift ?floor)
+                         (above ?floor ?above))
+                    :add((at ?lift ?above)
+                         (at ?P ?above))
+                    :del((at ?lift ?floor)
+                         (at ?P ?floor))
+                    :txt(?lift moves ?P up from ?floor to ?above)
+                    :cmd(ascend person)
+                    }
     going-down-filled{;moving the occupant down one floor
                       :pre((container ?lift)
-                            (agent ?P)
+                           (agent ?P)
                            (waiting ?P true)
-                            (contains ?lift ?P)
-                            (moving ?lift true)
-                            (at ?lift ?floor)
-                            (below ?floor ?below))
+                           (contains ?lift ?P)
+                           (moving ?lift true)
+                           (at ?lift ?floor)
+                           (below ?floor ?below))
                        :add((at ?lift ?below)
                             (at ?P ?below))
                        :del((at ?lift ?floor)
                             (at ?P ?floor))
                        :txt(?lift moves ?P down from ?floor to ?below)
                        :cmd(descend person)
-
                        }
-    stop-lift{:pre((container ?lift)
+    stop-lift{;stop the lift moving
+              :pre((container ?lift)
                     (moving ?lift true))
                :add((moving ?lift false))
                :del((moving ?lift true))
                :txt(?lift has stopped moving)
                :cmd(stop lift)
-
           }
-
-
-    wait-called{
+    wait-called{;wait for the lift to reach the floor the agent is on
                  :pre((container ?lift)
                       (agent ?P)
                       (waiting ?P false)
                       (at ?lift ?floor)
                       (at ?P ?p-floor)
-                      (moving ?lift true)
-
-                  )
+                      (moving ?lift true))
                  :add((waiting ?P true))
                  :del((waiting ?P false))
                  :txt(waiting for ?lift to reach ?p-floor floor)
                  :cmd(waiting at ?p-floor)
-
              }
-    enter{
-           :pre(
-
-                (container ?lift)
+    enter{;person enters the lift
+           :pre((container ?lift)
                 (agent ?person)
                 (contains ?lift nil)
                 (at ?person ?p-floor)
                 (at ?lift ?p-floor)
                 (waiting ?person true)
-                (moving ?lift false)
-                )
+                (moving ?lift false))
            :add((contains ?lift ?person)
-                (waiting ?person false)
-                )
+                (waiting ?person false))
            :del((contains ?lift nil)
                 (waiting ?person true))
            :txt(?person entered ?lift)
            :cmd(enter ?lift)
-
            }
-
-    select-floor{
-                  :pre(
-                       (agent ?person)
+    select-floor{;person selects the floor and lift starts moving
+                  :pre((agent ?person)
                        (container ?lift)
                        (contains ?lift ?person)
                        (moving ?lift false))
@@ -176,21 +152,19 @@
                   :txt(floor selected)
                   :cmd(select floor)
                   }
-    wait-selected{
-                   :pre(
-                           (agent ?person)
-                           (container ?lift)
-                          (contains ?lift ?person)
-                           (moving ?lift true)
-                           (waiting ?person false))
+    wait-selected{;person waits for lift to take them to their floor
+                   :pre((agent ?person)
+                        (container ?lift)
+                        (contains ?lift ?person)
+                        (moving ?lift true)
+                        (waiting ?person false))
                    :add((waiting ?person true))
                    :del((waiting ?person false))
                    :txt(waiting to reach selected floor)
                    :cmd(wait in lift)
                    }
-    exit{
-          :pre(
-               (container ?lift)
+    exit{;person exits the lift
+          :pre((container ?lift)
                (agent ?person)
                (contains ?lift ?person)
                (waiting ?person true)
